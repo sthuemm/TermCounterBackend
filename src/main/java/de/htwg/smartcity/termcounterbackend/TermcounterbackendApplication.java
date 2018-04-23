c5b61e466a3aa4ccc6d618ebcef799ec03f445f3
sethuemm <sethuemm@htwg-konstanz.de>
2018-04-23 14:52:46 +0200

    Initial commit

---
+++ src/main/java/de/htwg/smartcity/termcounterbackend/TermcounterbackendApplication.java
@@ -0,0 +1,12 @@
+package de.htwg.smartcity.termcounterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermcounterbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermcounterbackendApplication.class, args);
+	}
+}
